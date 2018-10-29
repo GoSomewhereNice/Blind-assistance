c60a198a95a1de790d50981fd58f814c9579d2ea
GoSomewhereNice <676536905@qq.com>
2018-10-29 06:01:08 +0000

    iBeacon send

--- Ultrasonic/loop.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-while :
-do
-	(python hc_sr04.py)
-	sleep 0.5
-done
---
+++ iBeaSender/iBeaSender.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo hciconfig hci0 noscan & sudo hciconfig hci0 leadv;
+sudo hcitool -i hci0 cmd 0x08 0x0008 1E 02 01 1A 1A FF 4C 00 02 15 8E 4B B3 30 D6 9F 11 E8 B5 68 08 00 20 0C 9A 66 00 00 00 00 C5 00
