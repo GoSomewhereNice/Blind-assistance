8f405c1543c8f572b75e7a91b379e14e65b1005d
GoSomewhereNice <676536905@qq.com>
2018-12-24 04:58:34 +0000

    Stop it when user arrived

---
+++ iBeaSender/stopBee.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+NAME=HC-SR501.py
+echo $NAME
+ID=`ps -ef | grep "$NAME" | grep -v "grep" | awk '{print $2}'`
+echo $ID
+echo "-------------"
+for id in $ID
+do
+sudo kill -9 $id
+echo "killed $id"
+done
+echo "-------------"
